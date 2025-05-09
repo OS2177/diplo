a91ee9f4520683249607d250650163ac54bbf723
OS2177 <85631879+OS2177@users.noreply.github.com>
2025-05-09 16:58:17 +0000

    Fix Home link and mobile pop-up menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -30,7 +30,6 @@ export default function Header() {
         <a href="https://diplo.cargo.site/" target="_blank" rel="noopener noreferrer">
           <h1 className="text-4xl font-extrabold text-black">diplo</h1>
         </a>
-
       </div>
 
       {/* Hamburger Menu Icon (Visible only on mobile) */}
@@ -138,13 +137,16 @@ export default function Header() {
       {/* Mobile Menu (Pop-up menu when Hamburger is clicked) */}
       {menuOpen && (
         <div className="lg:hidden absolute left-0 top-20 w-full bg-[#EEEDE5] shadow-lg z-50">
-          <NavLink
-            to="/"
+          {/* Home Link (External link) */}
+          <a
+            href="https://diplo.cargo.site/"
+            target="_blank"
+            rel="noopener noreferrer"
             className={`${linkClasses} block px-4 py-2`}
             onClick={() => setMenuOpen(false)}
           >
             Home
-          </NavLink>
+          </a>
           <NavLink
             to="/global-pulse"
             className={`${linkClasses} block px-4 py-2`}
