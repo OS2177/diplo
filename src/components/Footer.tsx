1f7f4207c6463fc640dc88f26c4970bdb274075c
OS2177 <85631879+OS2177@users.noreply.github.com>
2025-05-09 18:52:36 +0000

    Updated footer: 'diplo' and 'home' now link to https://diplo.cargo.site/

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,26 +4,28 @@ const linkClasses = 'text-gray-600 hover:text-black px-3 py-2 rounded-md text-sm
 
 export default function Footer() {
   return (
-    <footer className="bg-[#EEEDE5] py-6">
+    <footer className="bg-[#EEEDE5] py-6 mt-auto">
       <div className="max-w-2xl mx-auto text-center">
-        {/* Logo and Name */}
+        {/* Logo and Name with Link */}
         <div className="mb-4">
-          <img 
-            src="/images/diplo_logo.png" 
-            alt="Diplo Logo" 
-            className="h-14 mx-auto" 
-          />
-          <h1 className="text-2xl font-bold text-black mt-2">diplo</h1>
+          <a href="https://diplo.cargo.site/" target="_blank" rel="noopener noreferrer">
+            <img 
+              src="/images/diplo_logo.png" 
+              alt="Diplo Logo" 
+              className="h-14 mx-auto" 
+            />
+            <h1 className="text-2xl font-bold text-black mt-2">diplo</h1>
+          </a>
         </div>
 
         {/* Navigation Links */}
         <div className="space-x-6">
-          <NavLink
-            to="/"
+          <a
+            href="https://diplo.cargo.site/"
             className={linkClasses}
           >
             Home
-          </NavLink>
+          </a>
           <NavLink
             to="/global-pulse"
             className={linkClasses}
