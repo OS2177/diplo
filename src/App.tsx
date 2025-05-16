53023cbe8fd919e1048de529c18436b279399d2e
OS2177 <85631879+OS2177@users.noreply.github.com>
2025-05-16 18:31:03 +0000

    🔍 Test /admin-charts route with simple message

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
 import HomePage from './pages/HomePage';
 import CreateCampaignPage from './pages/CreateCampaignPage';
 import CampaignPage from './pages/CampaignPage';
